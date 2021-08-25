6052f30b8281a84984f5a56836423acc3e8e0625
Lilly Daniell <daniell.lilly@gmail.com>
2021-08-25 18:06:51 +0100

    Update error raise to 'fail' and combine release_bike tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,25 +3,18 @@ require_relative 'bike'
 class DockingStation
 
   attr_reader :bike 
+  # above line is same as:
   # def bike
   #   @bike
   # end
 
   def release_bike
-     Bike.new
+    fail "No bikes available" unless @bike
+    @bike
   end
 
   def dock_bike(bike)
     @bike = bike
   end
 
-  def do_not_release
-    raise StandardError.new "No bikes available"
-  end
-
 end 
-
-
-# .release_bike to give Bike.new object
-# Bike object to respond to method .working?
-# 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,27 +2,32 @@ require "docking_station"
 require 'bike'
 
 describe DockingStation do
+
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
   it { is_expected.to respond_to(:bike)}
   
-  it "release a bike" do 
-    bike = subject.release_bike 
-    expect(bike).to be_working #  "checks that bike is working" 
+  describe "#release_bike" do
+    it "#release a bike" do 
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(bike).to be_working # "checks that bike is working" 
+      expect(subject.release_bike).to eq bike 
+    end
   end
 
-  it "docks bike" do 
-    bike = Bike.new
-    expect(subject.dock_bike(bike)).to eq bike 
+  describe '#release_bike' do
+    it "raise an error if no bikes available" do
+    expect {subject.release_bike}.to raise_error("No bikes available")
+    end
   end
 
-  it "not to be released" do
-    expect {subject.do_not_release}.to raise_error("No bikes available")
-  end
-  # expect { raise "No bikes available" }.to raise_error
+  # it "docks bike" do 
+  #   bike = Bike.new
+  #   expect(subject.dock_bike(bike)).to eq bike 
+  # end
+
   # subject.dock_bike == DockingStation.dock_bike 
-
-
 
 end
     # expect(bike).to eq(:bike)
